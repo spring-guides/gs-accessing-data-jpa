137e9788bf30db7401cba12d92b026cc205b2605
Greg Turnquist <gturnquist@pivotal.io>
2015-09-28 09:27:03 -0500

    Polish up to use Java 8 lambda

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,52 +3,53 @@ package hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    @Autowired
-    CustomerRepository repository;
-    
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
 
-    @Override
-    public void run(String... strings) throws Exception {
-        // save a couple of customers
-        repository.save(new Customer("Jack", "Bauer"));
-        repository.save(new Customer("Chloe", "O'Brian"));
-        repository.save(new Customer("Kim", "Bauer"));
-        repository.save(new Customer("David", "Palmer"));
-        repository.save(new Customer("Michelle", "Dessler"));
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
 
-        // fetch all customers
-        log.info("Customers found with findAll():");
-        log.info("-------------------------------");
-        for (Customer customer : repository.findAll()) {
-            log.info(customer.toString());
-        }
-        System.out.println();
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+            log.info("");
 
-        // fetch an individual customer by ID
-        Customer customer = repository.findOne(1L);
-        log.info("Customer found with findOne(1L):");
-        log.info("--------------------------------");
-        log.info(customer.toString());
+			// fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+            log.info("");
 
-        // fetch customers by last name
-        log.info("Customer found with findByLastName('Bauer'):");
-        log.info("--------------------------------------------");
-        for (Customer bauer : repository.findByLastName("Bauer")) {
-            log.info(bauer.toString());
-        }
-    }
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Bauer")) {
+				log.info(bauer.toString());
+			}
+            log.info("");
+		};
+	}
 
 }
