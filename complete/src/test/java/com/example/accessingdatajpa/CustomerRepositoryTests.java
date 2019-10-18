04626cb1ff363a43e6e53b87e8ef0c91537c0291
Jay Bryant <jbryant@pivotal.io>
2019-10-18 12:25:37 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2, which necessitated upgrading to JUnit 5 and replacing the Gradle wrappers.

--- complete/src/test/java/com/example/accessingdatajpa/CustomerRepositoryTests.java
+++ complete/src/test/java/com/example/accessingdatajpa/CustomerRepositoryTests.java
@@ -20,14 +20,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class CustomerRepositoryTests {
 	@Autowired
---
+++ initial/src/test/java/com/example/accessingdatajpa/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
