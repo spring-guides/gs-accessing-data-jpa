bcdc5960534e871c77d1550a62f02c69ff28996d
Jay Bryant <jbryant@pivotal.io>
2019-10-21 11:38:13 -0500

    Test Changes
    Sam Brannen told me that I don't need the @ExtendWith annotation, because @DataJpaTest (and similar annotations) include @ExtendWith. Thanks, Sam! :)

--- complete/src/test/java/com/example/accessingdatajpa/CustomerRepositoryTests.java
+++ complete/src/test/java/com/example/accessingdatajpa/CustomerRepositoryTests.java
@@ -21,13 +21,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class CustomerRepositoryTests {
 	@Autowired
